f6185650fbf77fba27490a8431efc0de9f15bf91
金学权 <249792030@qq.com>
2020-12-17 15:08:22 +0800

    cli: cli

---
+++ src/views/list.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="list-rendering">
+    <ol>
+      <li v-for="(todo,index) in todos" :key="index">{{ todo.text }}</li>
+    </ol>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
+    }
+  }
+}
+</script>
